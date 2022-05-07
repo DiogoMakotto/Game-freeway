ba97339ec53a479841d057f35a0a754ba40164f5
MakottoSan <diogomakotto@gmail.com>
2022-05-07 18:29:23 -0300

    Criando estrutura e add imagens

---
+++ index.js
@@ -0,0 +1,13 @@
+let imgEstrada;
+
+function preload(){
+    imgEstrada = loadImage("./assets/estrada.png")
+}
+
+function setup() {
+    createCanvas(500, 400);
+}
+function draw() {
+    background(imgEstrada);
+    
+}
