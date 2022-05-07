3e2e68fc4cce247c74c9957cc1ac1c2944bc75d2
MakottoSan <diogomakotto@gmail.com>
2022-05-07 18:46:16 -0300

    Adioconando ator e carro

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 let imgEstrada;
+let imgAtor;
+let imgCarro;
 
 function preload(){
     imgEstrada = loadImage("./assets/estrada.png")
+    imgAtor = loadImage("./assets/ator-1.png")
+    imgCarro1 = loadImage("./assets/carro-1.png")
+    imgCarro2 = loadImage("./assets/carro-2.png")
+    imgCarro3 = loadImage("./assets/carro-3.png") 
 }
 
 function setup() {
@@ -9,5 +15,6 @@ function setup() {
 }
 function draw() {
     background(imgEstrada);
-    
-}
+    image(imgAtor, 100, 366, 30, 30)
+    image(imgCarro1, 420, 40, 50, 40)
+}   
