2961a438c3ce85d43aabbf371420951b05cb4b37
DiogoMakotto <diogomakotto@gmail.com>
2022-06-09 09:49:50 -0300

    add obstaculos

--- carro.js
+++ carro.js
@@ -1,27 +1,27 @@
 //código carro
 
-let xCarros = [600, 600, 600]
-let yCarros = [40, 96, 150]
-let velocidadeCarros = [2, 2.5, 3.6]
+let xCarros = [600, 600, 600, 600, 600, 600]
+let yCarros = [40, 96, 150, 210, 270, 318]
+let velocidadeCarros = [2, 2.5, 3.6, 5, 3.3, 2.3]
 let comprimentoCarro = 50;
 let alturaCarro = 40;
 
 //função para visualizar o carro
 function mostraCarro(){
-    for (let i = 0; i < imgCarros.length; i = i + 1 ){
+    for (let i = 0; i < imgCarros.length; i = i++){
     image(imgCarros[i], xCarros[i], yCarros[i], comprimentoCarro, alturaCarro);
     }
 }
 
 //velocidade
 function movimentaCarro(){
-    for(let i = 0; i < imgCarros.length; i = i + 1){
+    for(let i = 0; i < imgCarros.length; i = i++){
         xCarros[i] -= velocidadeCarros[i];
     }
 }
 
 function loopingPosicaoInicial() {
-    for(let i = 0; i < imgCarros.length; i = i + 1){
+    for(let i = 0; i < imgCarros.length; i = i++){
         if (passouTodaATela(xCarros[i])) {
         xCarros[i] = 600;
         }
